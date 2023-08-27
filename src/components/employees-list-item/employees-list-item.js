094b18276c34139c72c87ed568dd7ff5fa528ce5
Bilostenko <bilostenkod@gmail.com>
2023-08-27 12:19:25 +0300

    add dollar sign

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -19,29 +19,30 @@ const EmployeesListItem = (props) => {
 
   return (
     <li className={classNames}>
-    <span className="list-group-item-label like" onClick={onToggleProp} data-togle='rise'>{name}</span>
+      <span className="list-group-item-label like" onClick={onToggleProp} data-togle='rise'>{name}</span>
+      <div className="">
+        <input
+          type="text"
+          className="list-group-item-input"
+          value={salary}
+          onChange={salaryInputUpdate} />
+        $
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <button type="button"
+          className="btn-cookie btn-sm ">
+          <i className="fas fa-cookie"
+            onClick={onToggleProp}
+            data-togle='increase'></i>
+        </button>
 
-    <input 
-      type="text" 
-      className="list-group-item-input" 
-      value={salary} 
-      onChange={salaryInputUpdate} />
-      
-    <div className='d-flex justify-content-center align-items-center'>
-      <button type="button"
-        className="btn-cookie btn-sm ">
-        <i className="fas fa-cookie"
-          onClick={onToggleProp}
-          data-togle='increase'></i>
-      </button>
-
-      <button type="button"
-        className="btn-trash btn-sm ">
-        <i className="fas fa-trash" onClick={onDelete}></i>
-      </button>
-      <i className="fas fa-star"></i>
-    </div>
-  </li>
+        <button type="button"
+          className="btn-trash btn-sm ">
+          <i className="fas fa-trash" onClick={onDelete}></i>
+        </button>
+        <i className="fas fa-star"></i>
+      </div>
+    </li>
   )
 
 }
